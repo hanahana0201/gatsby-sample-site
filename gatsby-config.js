2a2a4bdb3134d0260282e72801d251382b077eb6
Masatoshi HANAI <masatoshi@Mac-mini.local>
2020-06-06 13:29:47 +0900

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        nama: `images`,
+        path: `${__dirname}/src/images/`,
+      }
+    }
+
+  ],
+}

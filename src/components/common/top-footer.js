fb3550bc0754ce80efba013f054b0ddeb0006dab
Masatoshi HANAI <masatoshi@mm2018.local>
2020-12-09 14:58:53 +0900

    Finish About Page.(desktop only).

---
+++ src/components/common/top-footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default (props) => (
+  <footer className={ `footer ${props.class}` }>
+      ©︎N’sクラフト設計室
+  </footer>
+)
---
+++ src/components/sections/section-hero.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Img from "gatsby-image"
+import Layout from "../common/layout"
+
+export default () => (
+  <section className="section is-hero">
+    <div className="box is-margin-horizontal-lg">
+      <figure>
+        <Img
+          fluid={data.flow.childImageSharp.fluid}
+          alt=""
+        />
+        <figcaption className="decorator">
+          <h1 className="project-heading-1">
+            家づくりの流れ
+          </h1>
+          <h2 className="project-heading-2">
+            あなたに<br/>
+            合わせます
+          </h2>
+        </figcaption>
+      </figure>
+    </div>
+  </section>
+)
